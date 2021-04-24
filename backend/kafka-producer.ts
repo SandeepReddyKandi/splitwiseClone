adc6ab61a168870fe92d99cade4f564a334611dc
SandeepReddyKandi <kandisandeepreddy98@gmail.com>
2021-04-24 19:12:13 +0530

    update kafka listener port

--- backend/kafka-producer.ts
+++ backend/kafka-producer.ts
@@ -3,7 +3,7 @@ import {Kafka} from "kafkajs";
 // the client ID lets kafka know who's producing the messages
 const clientId = "my-app"
 // we can define the list of brokers in the cluster
-const brokers = ["localhost:9093"]
+const brokers = ["localhost:9092"]
 // this is the topic to which we want to write messages
 const topic = "my-kafka-topic"
 
